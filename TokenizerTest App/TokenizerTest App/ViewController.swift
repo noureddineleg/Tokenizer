7b8a8fe84dbb7f89a3c7a0f85b8c5fd51b46f9e2
Nour Eddine Legragui <58888748+noureddineleg@users.noreply.github.com>
2024-01-07 16:42:46 +0100

    Initial Commit

---
+++ TokenizerTest App/TokenizerTest App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TokenizerTest App
+//
+//  Created by Nour-Eddine Legragui on 7/1/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
